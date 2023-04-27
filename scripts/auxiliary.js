297bd3bb5837d7be8a872a3b8a6c14e155a8ea49
KafedraTd <103263974+KafedraTd@users.noreply.github.com>
2023-04-27 21:42:15 +0300

    Add files via upload
    new21

--- scripts/auxiliary.js
+++ scripts/auxiliary.js
@@ -9,40 +9,58 @@ function glancePicture(type) {
     let fio = localStorage.getItem('f')
     let thead = document.querySelector('table').querySelector('thead')
     let tbody = document.querySelector('table').querySelector('tbody')
+
     document.title = 'Группа ' + group
     let specData = localStorage.getItem('d1')
     specData = JSON.parse(specData)
     let tchsData = localStorage.getItem('d2')
     tchsData = JSON.parse(tchsData)
-    console.log(specData)
     let studentsArray = Object.keys(specData[group])
     let subjects = Object.keys(specData[group][fio])
     let valuesPerSubject = {}
+    let countStudents={}
+
     let tr1 = document.createElement('tr')
     let td11 = document.createElement('td')
     td11.textContent = `Группа ${group}`
     tr1.appendChild(td11)
     for (let s of subjects) {
         valuesPerSubject[s] = 0
+        countStudents[s]=0
         let td12 = document.createElement('td')
         td12.classList.add('th')
         let dv12 = document.createElement('div')
+        dv12.className='pointsQty'
         dv12.id = s
+        let dvSt=document.createElement('div')
+        dvSt.className='studentsQty'
+        dvSt.id= s+'-s'
         td12.textContent = tchsData[s][1]
         let txt1 = tchsData[s][2]
         let txt2 = tchsData[s][0]
         td12.setAttribute('data-tooltip', txt1 + ' (' + txt2 + ')')
+        td12.appendChild(dvSt)
         td12.appendChild(dv12)
         tr1.appendChild(td12)
     }
     let td13 = document.createElement('td')
     td13.textContent = 'Итого не сдано'
-    let dv13 = document.createElement('div')
-    dv13.id = 'total'
-    td13.appendChild(dv13)
+    let dvStuds = document.createElement('div')
+    dvStuds.classList.add('studentsQty')
+    dvStuds.id = 'totalStud'
+
+    let dvPoint = document.createElement('div')
+    dvPoint.classList.add('pointsQty', 'ptQ')
+    dvPoint.id = 'total'
+    dvPoint.setAttribute('data-tooltip','Сортировать')
+    dvPoint.addEventListener('click',()=>{
+        sortTable(subjects.length+1)
+    })
+    td13.appendChild(dvStuds)
+    td13.appendChild(dvPoint)
     tr1.appendChild(td13)
     thead.appendChild(tr1)
-
+    totalPerGroupSt=0
     for (let fioLine of studentsArray) {
 
         let tr2 = document.createElement('tr')
@@ -135,6 +153,9 @@ function glancePicture(type) {
             }else if (type==1){
                 let totalPointsPerSubject=pValue[3]
                 let passedPoints=pValue[0]+pValue[1]
+                if (pValue[1]!=0){
+                    countStudents[s]++
+                }
                 for (i=0;i<totalPointsPerSubject;i++){
                     let circle = document.createElement('div')
                     if (pValue[2].includes(i+1)){
@@ -143,12 +164,13 @@ function glancePicture(type) {
                         greValueCount++
                     }else{
                         if (passedPoints<i+1){
-                            console.log(fio, s, passedPoints)
                             circle.classList.add('material-symbols-outlined', 'white', 'clickable')
                             circle.textContent = 'radio_button_checked'
+                            circle.setAttribute('data-tooltip', `Не наступила`)
                         }else{
                             circle.classList.add('material-symbols-outlined', 'red', 'clickable')
                             circle.textContent = 'radio_button_checked'
+                            circle.setAttribute('data-tooltip', `Не сдана`)
                             redValueCount++
                             valuesPerSubject[s]++
                         }
@@ -156,6 +178,7 @@ function glancePicture(type) {
                     }
                     td22.appendChild(circle)
                 }
+
                 
                 
 
@@ -163,7 +186,7 @@ function glancePicture(type) {
 
             tr2.appendChild(td22)
         }
-
+        if (redValueCount!=0){totalPerGroupSt++}
         let td23 = document.createElement('td')
         td23.textContent = redValueCount
         td23.id = `${course}-${group}-${fioLine}`
@@ -173,47 +196,118 @@ function glancePicture(type) {
         tbody.appendChild(tr2)
     }
     totalPerGroup = 0
+    
     for (let s of subjects) {
-        let dv = document.getElementById(s)
-        dv.textContent = valuesPerSubject[s]
+        let dv1 = document.getElementById(s)
+        dv1.textContent = valuesPerSubject[s]
+        txtId=s+'-s'
+        let dv0 = document.getElementById(txtId)
+        dv0.textContent = countStudents[s]
         totalPerGroup = totalPerGroup + valuesPerSubject[s]
         if (valuesPerSubject[s] == 0) {
-            dv.classList.add('countDivForSubjectGre')
+            dv1.classList.add('countDivForSubjectGre')
         } else {
-            dv.classList.add('countDivForSubjectRed')
+            dv1.classList.add('countDivForSubjectRed')
+        }
+        if (countStudents[s] == 0) {
+            dv0.classList.add('countDivForSubjectGre')
+        } else {
+            dv0.classList.add('countDivForSubjectRed')
         }
     }
     document.getElementById('total').textContent = totalPerGroup
+    document.getElementById('totalStud').textContent = totalPerGroupSt
     if (totalPerGroup == 0) {
         document.getElementById('total').classList.add('countDivForSubjectGre')
     } else {
         document.getElementById('total').classList.add('countDivForSubjectRed')
     }
+    if (totalPerGroupSt == 0) {
+        document.getElementById('totalStud').classList.add('countDivForSubjectGre')
+    } else {
+        document.getElementById('totalStud').classList.add('countDivForSubjectRed')
+    }
 }
 
-document.body.addEventListener('click', (e) => {
-    const target = e.target.closest('.clickable')
-    let id1 = target.id.split('=')[1]
-    let id2 = target.id.split('=')[0]
-    let element1 = document.getElementById(id1)
-    let element2 = document.getElementById(id2)
-    let number1 = Number(element1.textContent)
-    let number2 = Number(element2.textContent)
-    try {
-        if (target.classList.contains('gre')) {
-            target.classList.remove('gre')
-            target.classList.add('red')
-            target.textContent = 'radio_button_checked'
-            element1.textContent = `${number1 + 1}`
-            element2.textContent = `${number2 + 1}`
-        } else if (target.classList.contains('red')) {
-            target.classList.remove('red')
-            target.classList.add('gre')
-            target.textContent = 'check_box'
-            element1.textContent = `${number1 - 1}`
-            element2.textContent = `${number2 - 1}`
-        }
-        target.classList.add('yel')
-    } catch (e) { console.log(e) }
+// document.body.addEventListener('click', (e) => {
+//     const target = e.target.closest('.clickable')
+//     let id1 = target.id.split('=')[1]
+//     let id2 = target.id.split('=')[0]
+//     let element1 = document.getElementById(id1)
+//     let element2 = document.getElementById(id2)
+//     let number1 = Number(element1.textContent)
+//     let number2 = Number(element2.textContent)
+//     try {
+//         if (target.classList.contains('gre')) {
+//             target.classList.remove('gre')
+//             target.classList.add('red')
+//             target.textContent = 'radio_button_checked'
+//             element1.textContent = `${number1 + 1}`
+//             element2.textContent = `${number2 + 1}`
+//         } else if (target.classList.contains('red')) {
+//             target.classList.remove('red')
+//             target.classList.add('gre')
+//             target.textContent = 'check_box'
+//             element1.textContent = `${number1 - 1}`
+//             element2.textContent = `${number2 - 1}`
+//         }
+//         target.classList.add('yel')
+//     } catch (e) { console.log(e) }
 
-})
+// })
+
+function sortTable(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+
+    table = document.querySelector('table');
+    switching = true;
+    // Set the sorting direction to ascending:
+    dir = "asc";
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+      // Start by saying: no switching is done:
+      switching = false;
+      rows = table.rows;
+      /* Loop through all table rows (except the
+      first, which contains table headers): */
+      for (i = 1; i < (rows.length - 1); i++) {
+        // Start by saying there should be no switching:
+        shouldSwitch = false;
+        /* Get the two elements you want to compare,
+        one from current row and one from the next: */
+        x = rows[i].getElementsByTagName("TD")[n];
+        y = rows[i + 1].getElementsByTagName("TD")[n];
+        /* Check if the two rows should switch place,
+        based on the direction, asc or desc: */
+        if (dir == "asc") {
+          if (Number(x.innerHTML) > Number(y.innerHTML)) {
+            // If so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+          }
+        } else if (dir == "desc") {
+          if (Number(x.innerHTML) < Number(y.innerHTML)) {
+            // If so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+      if (shouldSwitch) {
+        /* If a switch has been marked, make the switch
+        and mark that a switch has been done: */
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        // Each time a switch is done, increase this count by 1:
+        switchcount ++;
+      } else {
+        /* If no switching has been done AND the direction is "asc",
+        set the direction to "desc" and run the while loop again. */
+        if (switchcount == 0 && dir == "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }
+  }
